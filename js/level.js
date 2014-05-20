78635ea6ac70c7ad0aa95db5f27ec82080e61a64
Samuel Storey <samstorey_@hotmail.co.uk>
2014-05-20 14:11:14 +0100

    initial commit

---
+++ js/level.js
@@ -0,0 +1,74 @@
+
+  var levelData = { 
+     1:  [[0,0,2,0,0,0,2,0,1,0,0],
+          [0,0,2,0,0,0,2,0,0,1,0],
+          [0,0,1,0,0,0,0,1,0,1,0],
+          [0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,1,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0],
+          [1,0,0,0,0,0,0,0,0,0,0],
+          [1,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,2,2,0,0,0,0],
+          [0,0,0,0,0,1,1,0,0,0,0]],
+     2:  [[0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,2,0,0,0,2,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0],
+          [1,0,0,0,0,0,0,0,0,0,0],
+          [0,0,2,2,2,2,2,2,2,2,0],
+          [0,0,2,2,2,2,2,2,2,2,0],
+          [0,0,1,1,1,1,1,1,1,1,0],
+          [0,0,1,1,1,1,1,1,1,1,0],
+          [0,0,1,1,1,1,1,1,1,1,0],
+          [0,0,1,1,1,1,1,1,1,1,0]] };
+
+// this changes how many alients per level. 1 is the white alien, 2 is the red one
+
+  var spriteData = {
+    'alien1': { sx: 0,  sy: 0,  w: 23, h: 18, cls: Alien, frames: 4 },
+    'alien2': { sx: 0,  sy: 18, w: 23, h: 18, cls: Alien, frames: 4 },
+    'player': { sx: 0,  sy: 36, w: 26, h: 17, cls: Player },
+    'missile': { sx: 0,  sy: 86, w: 3,  h: 14, cls: Missile }
+  }
+  
+//  positioning and adding new framing
+
+  function startGame() {
+    var screen = new GameScreen("Alien Invaders","press space to start",
+                                 function() {
+                                     Game.loadBoard(new GameBoard(1));
+                                 });
+    Game.loadBoard(screen);
+    Game.loop();
+  }
+
+  function endGame() {
+    var screen = new GameScreen("Game Over","(press space to restart)",
+                                 function() {
+                                     Game.loadBoard(new GameBoard(1));
+                                 });
+    Game.loadBoard(screen);
+  }
+
+
+  function winGame() {
+    var screen = new GameScreen("You Win!","(press space to restart)",
+                                 function() {
+                                     Game.loadBoard(new GameBoard(1));
+                                 });
+    Game.loadBoard(screen);
+  }
+
+  $(function() {
+    GameAudio.load({ 'fire' : 'media/laser.ogg', 'die' : 'media/explosion.ogg' }, 
+                   function() { 
+                       Game.initialize("#gameboard", levelData, spriteData,
+                                      { "start": startGame,
+                                        "die"  : endGame,
+                                        "win"  : winGame });
+                   });
+   });
+
+
+
